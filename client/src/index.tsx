ecef917f53f46a98349f9cebfb15e054e82d8fe0
firas-debich <debichf02@gmail.com>
2021-12-06 20:42:46 +0100

    delete the image from sign up component

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {ApolloClient , InMemoryCache , ApolloProvider} from "@apollo/client"
 import App from './component/App';
 import reportWebVitals from './reportWebVitals';
-
+console.log(process.env.REACT_APP_URI)
 const client = new ApolloClient({
   uri: process.env.REACT_APP_URI,
   cache : new InMemoryCache()
